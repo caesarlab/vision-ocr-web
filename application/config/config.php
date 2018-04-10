69ceda3a6b5f42d079e7def750abd32c826c57ea
Sherief Mursjidi <sherief@caesarlab.com>
2018-04-10 17:18:56 +0700

    Update config.php
    Change base_url value

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/google-vision';
+$config['base_url'] = 'http://phpstack-165851-479198.cloudwaysapps.com';
 
 /*
 |--------------------------------------------------------------------------
